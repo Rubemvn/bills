c65664d72ad8acae403765fdcdfae4e6bc8806a1
Rúbem Vieira <rubemvn17@gmail.com>
2024-12-14 21:53:22 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+	subsets: ['latin'],
+	weight: ['400', '500', '600', '700', '800', '900'],
+	display: 'swap',
+});
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {},
+			fontFamily: {
+				sans: [inter.style.fontFamily, 'Roboto'],
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
